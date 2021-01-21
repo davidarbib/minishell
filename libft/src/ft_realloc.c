36009f755993752b9f4487c6e42b6e404e8d08a2
fyusuf-a <fyusuf-a@student.42.fr>
2021-01-21 14:52:03 +0100

    ft_realloc can now shrink a memory space

--- libft/src/ft_realloc.c
+++ libft/src/ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: darbib <darbib@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/04 22:02:25 by darbib            #+#    #+#             */
-/*   Updated: 2020/12/05 20:00:44 by darbib           ###   ########.fr       */
+/*   Updated: 2021/01/21 14:47:12 by fyusuf-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,15 @@
 void	*ft_realloc(void *area, size_t in_bytes_n, size_t out_bytes_n)
 {
 	void	*p_out;
-	
-	if (!(p_out = ft_calloc(out_bytes_n, sizeof(unsigned char))))
+	size_t	bytes;
+
+	if (in_bytes_n > out_bytes_n)
+		bytes = out_bytes_n;
+	else
+		bytes = in_bytes_n;
+	if (!(p_out = ft_calloc(bytes, sizeof(unsigned char))))
 		return (NULL);
-	ft_memcpy(p_out, (const void *)area, in_bytes_n);
+	ft_memcpy(p_out, (const void *)area, bytes);
 	free(area);
 	return (p_out);
 }
