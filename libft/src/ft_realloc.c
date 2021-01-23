d543beb84bf58db18bb1d0ba5ba6308c9ba7b006
fyusuf-a <fyusuf-a@student.42.fr>
2021-01-23 15:20:25 +0100

    ft_realloc can shrink memory areas

--- libft/src/ft_realloc.c
+++ libft/src/ft_realloc.c
@@ -1,3 +1,4 @@
+
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -17,7 +18,8 @@
 void	*ft_realloc(void *area, size_t in_bytes_n, size_t out_bytes_n)
 {
 	void	*p_out;
-	
+	if (in_bytes_n > out_bytes_n)
+		in_bytes_n = out_bytes_n;
 	if (!(p_out = ft_calloc(out_bytes_n, sizeof(unsigned char))))
 		return (NULL);
 	ft_memcpy(p_out, (const void *)area, in_bytes_n);
