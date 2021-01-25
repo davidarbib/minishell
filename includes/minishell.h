ba9037d6df3a09832a31f75b701f097ab7bd617b
fyusuf-a <fyusuf-a@student.42.fr>
2021-01-25 19:25:39 +0100

    See here to test get_next_line not working

---
+++ includes/minishell.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fyusuf-a <fyusuf-a@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/21 14:13:58 by fyusuf-a          #+#    #+#             */
+/*   Updated: 2021/01/25 14:03:48 by fyusuf-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "libft.h"
+# include "lexer.h"
+# include "parser.h"
+# include <stdio.h>
+
+/*
+** g_env is a t_list of t_assignment
+*/
+
+t_list	*g_env;
+
+t_list	*lookup(char* key, t_list *assignments);
+
+#endif
