9fbc67f2841b1d91500a929912441f15a12459ca
fyusuf-a <fyusuf-a@student.42.fr>
2021-02-01 20:20:39 +0100

    Cleaning up debug

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: fyusuf-a <fyusuf-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/28 10:52:01 by fyusuf-a          #+#    #+#             */
-/*   Updated: 2021/02/01 20:14:15 by fyusuf-a         ###   ########.fr       */
+/*   Updated: 2021/02/01 20:20:15 by fyusuf-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,20 +25,16 @@ int		cmp(void *process, void *pid_ref)
 	return (((t_process*)process)->pid - *(int*)pid_ref);
 }
 
-void	launch(t_list *command, int fd)
+void	launch(t_list *command)
 {
 	char	**tab;
 	int		size;
-	int		debug = 0;
 
 	tab = (char**)ft_lsttotab(command, 8, &size);
 	/*ft_lstclear(command, del);*/
 	tab[size] = 0;
-	if (debug)
-		dprintf(fd, "-> %s\n", tab[0]);
 	execve((char*)tab[0], (char*const*)tab, NULL);
 	perror("minishell");
-	/*pause();*/
 }
 
 /*
@@ -48,19 +44,15 @@ void	launch(t_list *command, int fd)
 int		i = 0;
 t_list	*all_childs;
 
-int		eval(t_pipeline *pipeline, int pipe_stdin, int fd)
+int		eval(t_pipeline *pipeline, int pipe_stdin)
 {
 	int	pid;
 	int p[2];
 	int next_stdin;
 	int	status;
-	int debug = 1;
 	/*int	*pid_index;*/
 	t_process	*process;
 
-	++i;
-	/*if (debug)*/
-		/*dprintf(fd, "pass no %d, %s\n", ++i, pipeline?"content":"null" );*/
 	if (!pipeline)
 		return (0);
 	process = malloc(sizeof(t_process));
@@ -68,8 +60,6 @@ int		eval(t_pipeline *pipeline, int pipe_stdin, int fd)
 	if (pipeline->next)
 	{
 		pipe(p);
-		if (debug)
-			dprintf(fd, "creating pipe %d : %d <- %d\n", i, p[0], p[1]);
 		next_stdin = p[0];
 		process->pipe_out = p[1];
 	}
@@ -78,23 +68,16 @@ int		eval(t_pipeline *pipeline, int pipe_stdin, int fd)
 	{
 		if (pipe_stdin != -1)
 		{
-			if (debug)
-				dprintf(fd, "-- child %d: redirecting input to fd no %d\n", i, pipe_stdin);
 			dup2(pipe_stdin, 0);
 			close(pipe_stdin);
-			dprintf(fd, "child %d: closing %d\n", i, pipe_stdin); 
 		}
 		if (pipeline->next)
 		{
-			dprintf(fd, "child %d: closing %d\n", i, p[1]); 
 			dup2(p[1], 1);
 			close(p[1]);
-			if (debug)
-				dprintf(fd, "-- child %d: redirecting output to fd no %d\n", i, p[1]);
 			close(p[0]);
 		}
-		dprintf(fd, "child %d: closing %d\n", i, p[0]); 
-		launch(((t_simple_command*)pipeline->content)->args, fd);
+		launch(((t_simple_command*)pipeline->content)->args);
 		return (0);
 	}
 	else if (pid < 0)
@@ -104,66 +87,35 @@ int		eval(t_pipeline *pipeline, int pipe_stdin, int fd)
 		close(pipe_stdin);
 	process->pid = pid;
 	ft_lstadd_back_elem(&all_childs, process);
-	if (debug)
-		dprintf(fd, "-- child %d's pid is %d\n", i, pid);
 	if (pipeline->next)
 	{
-		if (debug)
-			dprintf(fd, "parent %d: giving %d as stdin for next child\n", i, next_stdin);
-		return (eval(pipeline->next, next_stdin, fd));
+		return (eval(pipeline->next, next_stdin));
 	}
 	return (pid);
 }
 
-int		run_once(t_reader *reader, char *line, int fd)
+int		run_once(t_reader *reader, char *line)
 {
 	parse(reader, line);
-	return (eval(reader->parser.current_pipeline, -1, fd));
+	return (eval(reader->parser.current_pipeline, -1));
 }
 
-void	wait_all_childs(int pid, int fd)
+void	wait_all_childs()
 {
-	(void)pid;
 	int			status;
 	t_list		*temp;
 	int			stopped_pid;
 	t_process	*process;
-	int			debug = 1;
 
 	temp = all_childs;
-	if (debug)
-	{
-		printf("initial pids:");
-		while (temp)
-		{
-			printf("%d - ", *((int*)temp->content));
-			temp = temp->next;
-		}
-		printf("\n");
-	}
 	while (1)
 	{
 		stopped_pid = waitpid(-1, &status, WUNTRACED);
 		process = ft_lstfind_content(all_childs, &stopped_pid, cmp);
-		if (debug)
-			dprintf(fd, "closing fd %d of process %d\n", process->pipe_out, stopped_pid);
 		ft_lstremove_if(&all_childs, &stopped_pid, cmp, del);
 		temp = all_childs;
-		if (debug)
-		{
-			printf("now:");
-			if (!temp)
-			{
-				printf("nothing\n");
-				break;
-			}
-			while (temp)
-			{
-				printf("%d - ", ((t_process*)(temp->content))->pid);
-				temp = temp->next;
-			}
-			printf("\n");
-		}
+		if (temp)
+			break;
 		/*if (WIFEXITED(status) || WIFSIGNALED(status))*/
 			/*break ;*/
 	}
@@ -175,11 +127,9 @@ int		main(int argc, char **argv)
 	int			result;
 	t_reader	reader;
 	int			pid;
-	int			fd;
 
 	(void)argv;
 	remove("toto.txt");
-	fd = open("toto.txt", O_CREAT | O_WRONLY, 0666);
 	pid = 0;
 	if (argc == 1)
 	{
@@ -196,14 +146,14 @@ int		main(int argc, char **argv)
 				exit(EXIT_SUCCESS);
 			}*/
 			parse(&reader, line);
-			pid = eval(reader.parser.current_pipeline, -1, fd);
-			wait_all_childs(pid, fd);
+			pid = eval(reader.parser.current_pipeline, -1);
+			wait_all_childs();
 		}
 	}
 	else	 // for testing
 	{
-		pid = run_once(&reader, argv[1], fd);
-		wait_all_childs(pid, fd);
+		pid = run_once(&reader, argv[1]);
+		wait_all_childs();
 	}
 	return (0);
 }
