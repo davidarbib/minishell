e88f740cf4f62bb67aa9edd64d95c2a30de37df0
fyusuf-a <fyusuf-a@student.42.fr>
2021-02-20 18:22:29 +0100

    SIGQUIT corrected

--- srcs/signals.c
+++ srcs/signals.c
@@ -6,7 +6,7 @@
 /*   By: fyusuf-a <fyusuf-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 11:51:31 by fyusuf-a          #+#    #+#             */
-/*   Updated: 2021/02/20 16:54:58 by fyusuf-a         ###   ########.fr       */
+/*   Updated: 2021/02/20 18:22:09 by fyusuf-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ void	signal_handler(int signal)
 	tmp = g_all_childs;
 	if (!tmp)
 	{
-		write(2, "\nminishell-1.0$ ", 16);
+		if (signal != SIGINT)
+			write(2, "\nminishell-1.0$ ", 16);
 		g_last_command_result = 130;
 		return ;
 	}
